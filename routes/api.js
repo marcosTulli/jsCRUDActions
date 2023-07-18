05e537cf495b01d825e401d5201c0c1762f740f6
Marcos <marcos.tulli@raona.com>
2023-07-18 12:52:00 -0300

    Add: Initial Actions

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,8 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const productsRouter = require("./products");
+const productsRouter = require('./products');
 
 module.exports = function (db) {
   router.use(productsRouter(db));
-
   return router;
 };
--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,36 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
 
+const path = {
+  products: '/products',
+  id: '/:id',
+};
+
 module.exports = function (db) {
+  router
+    .route(path.products)
+    .get((req, res) => {
+      res.send(db.get('products').value());
+    })
+    .post((req, res) => {
+      const newProduct = req.body;
+      res.send(db.get('products').insert(newProduct).write());
+    });
+
+  router
+    .route(`${path.products}${path.id}`)
+    .patch((req, res) => {
+      res.send(
+        db.get('products').find({ id: req.params.id }).assign(req.body).write()
+      );
+    })
+    .delete((req, res) => {
+      db.get('products').remove({ id: req.params.id }).write();
+      res.status(204).send();
+    })
+    .get((req, res) => {
+      res.send(db.get('products').find({ id: req.params.id }).value());
+    });
+
   return router;
 };
